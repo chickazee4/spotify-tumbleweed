aff9581a6c93937f0eb60aa7eca28214a39cde1c
lyndsay <lyndsay@vivaldi.net>
2021-12-01 08:23:29 -0700

    Update getspotify.sh

--- getspotify.sh
+++ getspotify.sh
@@ -4,6 +4,6 @@ cd /tmp
 wget https://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${VERSION}_amd64.deb
 ar x spotify-client*.deb
 tar xf data.tar.gz
-cp usr/* /usr
+cp -r usr/* /usr
 patchelf --replace-needed libcurl-gnutls.so.4 libcurl.so.4 /usr/bin/spotify
 ln -s /usr/share/spotify/spotify.desktop /usr/share/applications/spotify.desktop
