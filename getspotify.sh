59a0948d0cb78117a7a811cabb78f75f9c81d75b
chickazee4 <lyndsay@vivaldi.net>
2021-12-01 08:26:46 -0700

    somehow missed this

--- getspotify.sh
+++ getspotify.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ $1 == 'rm' ]; then
+	rm -rf /usr/bin/spotify /usr/share/spotify /usr/share/doc/spotify-client
+fi
 VERSION=1.1.72.439.gc253025e
 cd /tmp
 wget https://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${VERSION}_amd64.deb
